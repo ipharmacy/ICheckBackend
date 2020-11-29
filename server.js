7f3838ef4dc656a22c88467e4b5bbaa5f69e05e3
Mehdi Behira <mehdibehira@gmail.com>
2020-11-29 16:11:35 +0100

    test-monitoring

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const connectDB = require('./dB/connection.js')
+const scout = require("@scout_apm/scout-apm");
+
+// The "main" function
+async function start() {
+  // Trigger the download and installation of the core-agent
+  await scout.install({
+    allowShutdown: true, // allow shutting down spawned scout-agent processes from this program
+    monitor: true, // enable monitoring
+    name: "",
+    key: "",
+  });
+
 const app = express();
 
 connectDB();
@@ -17,3 +29,20 @@ app.use('/api/products',require('./Controllers/productController'))
 const Port = process.env.Port || 3000
 app.listen(process.env.PORT || 3000)
 app.listen(Port,() => console.log('server started'))
+
+
+
+
+  // Enable the app-wide scout middleware
+  app.use(scout.expressMiddleware());
+
+  // Add other middleware and routes
+  // app.use( ... )
+  // app.get( ... )
+
+  // Start express
+  app.start();
+}
+
+// If this script is executed directly, run the start function
+if (require.main === module) { start(); }
